9251dec2c3628869cd2a2c43d2b7955486bde3ec
Rory Hellier <rory.hellier@googlemail.com>
2019-11-26 11:00:24 -0500

    Saving and creating new branch to remove mqs

--- JS/navbarAction.js
+++ JS/navbarAction.js
@@ -2,12 +2,15 @@
 const html = document.getElementById('html');
 const search = document.getElementById('navbarSearch');
 const nav = document.getElementById('nav');
+const container = document.getElementById('navbarContainer');
 search.addEventListener("click", function (ev) {
+  container.style.zIndex = "12";
   search.style.width = "91%";
   search.style.transition = "width 0.3s ease";
   ev.stopPropagation(); //this is important! If removed, you'll get both alerts
 });
 html.addEventListener("click", function() {
+    container.style.zIndex = "1";
     search.style.width = "60px";
 });
 
