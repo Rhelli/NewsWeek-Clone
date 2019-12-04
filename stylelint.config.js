3da71c172a1b53f508297988019d16725af5f7e0
Rory Hellier <rory.hellier@googlemail.com>
2019-12-04 17:26:17 -0500

    solved stickler errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -105,5 +105,5 @@ module.exports = {
     "value-list-comma-space-after": "always-single-line",
     "value-list-comma-space-before": "never",
     "value-list-max-empty-lines": 0,
-  },
+  }, ignore: ["selectors-within-list"],
 }
